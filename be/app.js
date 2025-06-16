78e09d42039bef93ae3544ed282cd20cc75fa9af
vuden2605 <vuden2605@gmail.com>
2025-06-16 18:26:54 +0700

    up

---
+++ be/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+//const { connectDB } = require('./config/db.config');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+// Kết nối đến cơ sở dữ liệu
+//connectDB()
+// Route mặc định
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+// Khởi động server
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
